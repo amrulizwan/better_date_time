155df5e1384342b94b5147ff9d0e2be3b24769d5
Amrul Izwan <amrulizwan7@gmail.com>
2024-06-04 03:43:54 +0800

    update

--- lib/better_date_time.dart
+++ lib/better_date_time.dart
@@ -1,12 +1,10 @@
 library better_date_time;
 
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:intl_translation/generate_localized.dart';
 import 'package:intl/intl.dart';
 
 class BetterDateTime {
-  formattedDate() async {
-    await initializeDateFormatting('id_ID', null);
-    return DateFormat('d MMMM yyyy', 'id_ID').format(DateTime.now());
+  formattedDate(String locale) {
+    return DateFormat('d MMMM yyyy', locale).format(DateTime.now());
   }
 }
